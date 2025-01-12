d819d72cae4861f23ce5daccde227f9a9961a34f
Rucheelt <narsingraosto@gmail.com>
2025-01-12 18:11:27 +0530

    operations in lists

---
+++ operations on lists.py
@@ -0,0 +1,10 @@
+a=["rucheel", "codingal", 55 , "3fr" , "laptop", "88"]
+print(a)
+a.append("july")
+print(a)
+a.pop(0)
+print(a)
+a.remove("july")
+print(a)
+a=[55 , "kiwi" , 856 , "cycle"]
+print(a[0:3])
